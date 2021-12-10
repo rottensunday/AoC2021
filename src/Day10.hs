419ab1f1e5dbd52dd36b036f39846ff0c5377eed
rotten <arnachimm@gmail.com>
2021-12-10 18:33:39 +0100

    Day10 better readability

--- src/Day10.hs
+++ src/Day10.hs
@@ -6,6 +6,9 @@ import qualified Data.Text as T
 import FileAccess
 import Data.Maybe (fromJust, fromMaybe)
 
+type ErrorChar = Char
+type CharsToMatch = [Char]
+
 inputs = fmap T.unpack <$> readLines "resources/Day10Input.txt"
 
 legalOpeningChars = ['(', '[', '{', '<']
@@ -21,20 +24,22 @@ autocompletePoints c = fromMaybe 0 $ lookup c [(')', 1), (']', 2), ('}', 3), ('>
 calculateAutocompletePoints :: String -> Integer
 calculateAutocompletePoints = foldl (\acc c -> acc * 5 + autocompletePoints c) 0
 
-closingChars :: String -> Either Char String
-closingChars = foldl processChar (Right [])
+-- processes input string, returning ErrorChar if given invalid closing char, or CharsToMatch
+-- with collection of chars that need to be matched but EOF appeared
+processChunks :: String -> Either ErrorChar CharsToMatch
+processChunks = foldl processChar (Right [])
   where
-    processChar :: Either Char String -> Char -> Either Char String
-    processChar (Left c) _ = Left c
+    processChar :: Either ErrorChar CharsToMatch -> Char -> Either ErrorChar CharsToMatch
+    processChar (Left c) _ = Left c -- Error is propagated
     processChar (Right []) c = if c `elem` legalOpeningChars then Right [c] else Left c
     processChar (Right (x : xs)) c
-      | c `elem` legalOpeningChars = Right (c : x : xs)
-      | c == legalClosingChar x = Right xs
+      | c `elem` legalOpeningChars = Right (c : x : xs) -- If c is opening, add to be matched
+      | c == legalClosingChar x = Right xs -- If c is closing and matches opening, then trim head of to be matched
       | otherwise = Left c
 
-day10Task1 = fmap ((sum . map syntaxErrorPoints . lefts) . fmap closingChars) inputs
+day10Task1 = fmap ((sum . map syntaxErrorPoints . lefts) . fmap processChunks) inputs
 
 day10Task2 = fmap (\l -> let results = getResults l in results !! (length results `div` 2)) inputs
   where
     getResults :: [String] -> [Integer]
-    getResults = sort . (map (calculateAutocompletePoints . map legalClosingChar) . rights) . map closingChars
+    getResults = sort . (map (calculateAutocompletePoints . map legalClosingChar) . rights) . map processChunks
